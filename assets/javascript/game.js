bc95430264f27742710b7ed58408316fff9f11f7
Ethan Zander <ethanzander@users.noreply.github.com>
2017-07-12 15:47:11 -0600

    Initial Files
    Setup files for the project.

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+$( document ).ready(function() {
+//Variables
+
+// Set Game values
+
+//Set all Functions
+
+  //Reset game variables
+  
+  // Reset Game values
+  
+
+// Game logic
+
+//End document.ready
+});
