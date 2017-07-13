a811d216f401301ad102d77e8053a0d801ebe0b9
Ethan Zander <ethanzander@users.noreply.github.com>
2017-07-13 16:40:00 -0600

    Initial Framework
    Setup the initial framework and some .js  functionality.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,25 @@
 
 $( document ).ready(function() {
 //Variables
+setTimeout(tenSeconds, 1000 * 10);
+setTimeout(fiveSeconds, 1000 * 5);
+
 
 // Set Game values
 
 //Set all Functions
+function start() {
+  if () {}
+}
+function tenSeconds() {
+  $('#time-left').html('<h3>You have 10 seconds left!</h3>');
+}
+function fiveSeconds() {
+  $('#time-left').html('<h3>You have 5 seconds left!</h3>');
+}
+function timeUp() {
+  $('#time-left').html('<h3>Your time is UP!</h3>');
+}
 
   //Reset game variables
   
