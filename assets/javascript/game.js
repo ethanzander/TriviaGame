5eb040e2809ae4c0c72064ea31f14a79be5b882b
Ethan Zander <ethanzander13@gmail.com>
2017-07-14 12:55:57 -0600

    12:55 071417

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,62 @@
 
 $( document ).ready(function() {
 //Variables
-setTimeout(tenSeconds, 1000 * 10);
-setTimeout(fiveSeconds, 1000 * 5);
+// setTimeout(tenSeconds, 1000 * 10);
+// setTimeout(fiveSeconds, 1000 * 5);
 
 
 // Set Game values
+var number = 30;
+var intervalId;
 
 //Set all Functions
-function start() {
-  if () {}
-}
+
 function tenSeconds() {
-  $('#time-left').html('<h3>You have 10 seconds left!</h3>');
+	if (number === 10) {
+  		$('#time-left').html("<h3>You have 10 seconds left!</h3>");
+  	}
 }
-function fiveSeconds() {
-  $('#time-left').html('<h3>You have 5 seconds left!</h3>');
+// function fiveSeconds() {
+// 	if (number === 5) {}
+//   		$('#time-left').html("<h3>You have 5 seconds left!</h3>");
+//   	}
+// }
+// function timeUp() {
+// 	if (number === 0) {}
+//   		$('#time-left').html('<h3>Your time is UP!</h3>');
+//   	}
+// }
+function run() {
+  if (!intervalId) {
+    intervalId = setInterval(decrement, 1000);
+  }
 }
-function timeUp() {
-  $('#time-left').html('<h3>Your time is UP!</h3>');
+function decrement() {
+  number--;
+  $("#show-number").html("<h1>" + number + "</h1>");
+    if (number === 0) {
+      stop();
+      alert("Time Up!");
+    }
+}
+function stop() {
+  clearInterval(intervalId);
+  intervalId = undefined;
 }
 
-  //Reset game variables
-  
-  // Reset Game values
-  
+  // alert("Your Results");
 
 // Game logic
+$('#show-number').html('<h1>30</h1>');
+$('#start-button').on("click", function(){
+  run();
+});
+$('#finished').on("click", function(){
+  stop();
+});
 
 //End document.ready
 });
+
+
+
